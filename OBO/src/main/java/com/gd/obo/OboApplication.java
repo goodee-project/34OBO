fe7872b3fc552942c5b9114fef87acdbbf6e49c8
briana9504 <briana9504@naver.com>
2021-07-12 11:29:08 +0900

    OBO 프로젝트 초기화
    pom.xml, application.properties

---
+++ OBO/src/main/java/com/gd/obo/OboApplication.java
@@ -0,0 +1,13 @@
+package com.gd.obo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OboApplication.class, args);
+	}
+
+}
---
+++ OBO/src/test/java/com/gd/obo/OboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.obo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
