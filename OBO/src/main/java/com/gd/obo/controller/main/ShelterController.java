26fe07359056ff79f9b3294a1e21a328c3230e5a
sec <sec@DESKTOP-E26LMGC>
2021-07-13 20:39:15 +0900

    [김선유] shelter 리스트, 상세보기 구현중 vo -shelter 추가
    Mapper
    ShelterMapper : 보호소 리스트 쿼리, 상세보기 쿼리
    
    Service
    ShelterService: 보호소 리스트, 상세보기
    
    Controller
    shelterController: shelter 리스트, 상세보기 구현중
    
    View
    -getShetlerlist : 보호소 리스트 구현중

---
+++ OBO/src/main/java/com/gd/obo/controller/main/ShelterController.java
@@ -0,0 +1,42 @@
+// 작성자: 김선유
+package com.gd.obo.controller.main;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gd.obo.restapi.main.ShelterService;
+import com.gd.obo.vo.main.Shelter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class ShelterController {
+	@Autowired ShelterService shelterService;
+	
+	// shelter 리스트
+	@GetMapping("/getShelterList")
+	public String getShelterlist(Model model) {
+		List<Shelter> shelterList = shelterService.getShelterList();
+		log.debug("@@@@@ shelterList: "+shelterList);
+		return "getShelterList";
+		
+	}
+	
+	// shelter 상세보기
+	@GetMapping("/getShelterOne")
+	public String getShelterOne(Model model,
+									@RequestParam(value="shelterId",required= true) int shelterId) {
+		Map<String, Object> map = shelterService.getShelterOne(shelterId);
+		log.debug("@@@@@ map: "+map);
+		model.addAttribute("shelterMap", map.get("shelterMap"));
+		return "getShelterOne";
+	}
+}
+
---
+++ OBO/src/main/java/com/gd/obo/vo/main/Shelter.java
@@ -0,0 +1,16 @@
+package com.gd.obo.vo.main;
+
+import lombok.Data;
+
+@Data
+public class Shelter {
+	private int shelterId;
+	private int addressId;
+	private String shelterName;
+	private String createDate;
+	private String introduction;
+	private int protectDuration;
+	private String shelterPhone;
+	private String account;
+	private int active;
+}
