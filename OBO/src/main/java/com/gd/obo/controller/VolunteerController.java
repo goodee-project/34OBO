562a286a44db5c3b2de216005533468ed163f89c
yunjeomi <yunjeoming@gmail.com>
2021-07-19 14:26:44 +0900

    [이윤정] staff volunteer & adoptAndCare jsp 템플릿 적용중

--- OBO/src/main/java/com/gd/obo/controller/VolunteerController.java
+++ OBO/src/main/java/com/gd/obo/controller/VolunteerController.java
@@ -2,8 +2,13 @@
 
 package com.gd.obo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.gd.obo.vo.Staff;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,16 +22,57 @@ public class VolunteerController {
 		return "staff/getVolunteer";
 	}
 	
-	
-	// staff 일반봉사
-	@GetMapping("/staff/getVolunteerN")
-	public String getVolunteerN() {
-		return "staff/getVolunteerN";
+	// staff 일반봉사 - 모집공고
+	@GetMapping("/staff/getVolunteerRecruitN")
+	public String getVolunteerRecruitN(HttpSession session, Model model) {
+		int shelterId = ((Staff)(session.getAttribute("loginStaff"))).getShelterId();
+		log.debug("●●●●▶shelterId: "+shelterId);
+		
+		return "staff/getVolunteerRecruitN";
 	}
 	
-	// staff 정기봉사
-	@GetMapping("/staff/getVolunteerP")
-	public String getVolunteerP() {
-		return "staff/getVolunteerP";
+	// staff 일반봉사 - 모집공고
+	@GetMapping("/staff/getVolunteerApplyN")
+	public String getVolunteerApplyN(HttpSession session, Model model) {
+		int shelterId = ((Staff)(session.getAttribute("loginStaff"))).getShelterId();
+		log.debug("●●●●▶shelterId: "+shelterId);
+		
+		return "staff/getVolunteerApplyN";
+	}
+	
+	// staff 일반봉사 - 확인목록
+	@GetMapping("/staff/getVolunteerCheckN")
+	public String getVolunteerCheckN(HttpSession session, Model model) {
+		int shelterId = ((Staff)(session.getAttribute("loginStaff"))).getShelterId();
+		log.debug("●●●●▶shelterId: "+shelterId);
+		
+		return "staff/getVolunteerCheckN";
+	}
+	
+	// staff 정기봉사 - 모집공고
+	@GetMapping("/staff/getVolunteerRecruitP")
+	public String getVolunteerRecruitP(HttpSession session, Model model) {
+		int shelterId = ((Staff)(session.getAttribute("loginStaff"))).getShelterId();
+		log.debug("●●●●▶shelterId: "+shelterId);
+		
+		return "staff/getVolunteerRecruitP";
+	}
+	
+	// staff 정기봉사 - 모집공고
+	@GetMapping("/staff/getVolunteerApplyP")
+	public String getVolunteerApplyP(HttpSession session, Model model) {
+		int shelterId = ((Staff)(session.getAttribute("loginStaff"))).getShelterId();
+		log.debug("●●●●▶shelterId: "+shelterId);
+		
+		return "staff/getVolunteerApplyP";
+	}
+	
+	// staff 정기봉사 - 확인목록
+	@GetMapping("/staff/getVolunteerCheckP")
+	public String getVolunteerCheckP(HttpSession session, Model model) {
+		int shelterId = ((Staff)(session.getAttribute("loginStaff"))).getShelterId();
+		log.debug("●●●●▶shelterId: "+shelterId);
+		
+		return "staff/getVolunteerCheckP";
 	}
 }
