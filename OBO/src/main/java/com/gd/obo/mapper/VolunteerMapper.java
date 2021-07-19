a48f1cd66356d0aafc25df8f7b20fb4233ec86eb
coke0788 <coke0788@naver.com>
2021-07-19 18:15:28 +0900

    [남궁혜영] view 디자인 일부 수정, 회원용 일반봉사 모집 리스트 구현 Mapper VolunteerMapper : 회원용 일반봉사 목록 구현
    Service
    VolunteerService : 회원용 일반봉사 목록 구현
    
    Controller
    VolunteerController : 회원용 일반봉사 목록 구현, 봉사 메인 페이지 구현
    
    View
    getVolunteer : 회원용 봉사 메인 페이지 구현
    getVolunteer : 회원용 봉사 목록 페이지 검색, 페이징 포함 구현, 신청은 미구현이며 인원 풀일 경우 경고창 출력 기능
    구현.
    MainMenu : 링크 변경

--- OBO/src/main/java/com/gd/obo/mapper/VolunteerMapper.java
+++ OBO/src/main/java/com/gd/obo/mapper/VolunteerMapper.java
@@ -1,5 +1,15 @@
 package com.gd.obo.mapper;
 
-public interface VolunteerMapper {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gd.obo.vo.Page;
+
+@Mapper
+public interface VolunteerMapper {
+	List<Map<String, Object>> selectVolunteerNList(Map<String, Object> map);
+	int selectVolunteerNTotal(String searchWord);
+	List<String> selectVolunteerCategory();
 }
